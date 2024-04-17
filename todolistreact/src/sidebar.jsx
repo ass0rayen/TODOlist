2187f8eda1fca4d9c3336dbf13a0162c6c29a7a4
ass0rayen <77028369+ass0rayen@users.noreply.github.com>
2024-04-17 12:30:59 +0200

    completed sidebar

--- todolistreact/src/sidebar.jsx
+++ todolistreact/src/sidebar.jsx
@@ -1,14 +1,14 @@
 import SideButton from "./button"
-import {BoltOutlined,PriorityHighOutlined,CalendarTodayOutlined,CheckOutlined,HistoryOutlined} from '@mui/icons-material';
+import {LightOutlined,PriorityHighOutlined,CalendarTodayOutlined,CheckOutlined,HistoryOutlined} from '@mui/icons-material';
 
 const Sidebar= ()=>{
     return(
         <div className="sideBar">
-            <><SideButton name="Today" icon = {<BoltOutlined/>}></SideButton></>
-            <><SideButton icon = {<PriorityHighOutlined/>} name="Important"></SideButton></>
-            <><SideButton icon = {<CalendarTodayOutlined/>}name="Calendar"></SideButton></>
-            <><SideButton icon={<CheckOutlined/>} name="Completed Recently"></SideButton></>
-            <><SideButton icon ={<HistoryOutlined/>} name="today"></SideButton></>
+            <><SideButton name="Today" icon = {<LightOutlined className="icon1"/>}></SideButton></>
+            <><SideButton icon = {<PriorityHighOutlined className="icon2" />} name="Important"></SideButton></>
+            <><SideButton icon = {<CalendarTodayOutlined className="icon3"/>} name="Calendar"></SideButton></>
+            <><SideButton icon={<CheckOutlined className="icon4" />} name="Completed"></SideButton></>
+            <><SideButton icon ={<HistoryOutlined className="icon5" />} name="today"></SideButton></>
         </div>
     )
 }
